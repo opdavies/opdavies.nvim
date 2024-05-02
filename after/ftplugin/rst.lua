0d142447d5796801b4bf70f596a501c654804f9c
Oliver Davies <oliver@oliverdavies.dev>
2024-05-02 20:11:50 +0100

    Add `path` completion for rst files

--- after/ftplugin/rst.lua
+++ after/ftplugin/rst.lua
@@ -1,3 +1,10 @@
 local opt = vim.opt
 
 opt.wrap = true
+
+local cmp = require "cmp"
+local sources = cmp.get_config().sources
+
+table.insert(sources, { name = "path" })
+
+cmp.setup.buffer { sources = sources }
