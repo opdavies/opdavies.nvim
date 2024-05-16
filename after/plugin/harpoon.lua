c40a03a45653bd0e9af9ea38eb990d003ae42b41
Oliver Davies <oliver@oliverdavies.dev>
2024-05-16 18:09:49 +0100

    Update Harpoon configuration
    Replace usages of custom keybinding functions with `vim.keymap.set()`.

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,21 +3,16 @@ require("harpoon").setup()
 local mark = require "harpoon.mark"
 local ui = require "harpoon.ui"
 
-local nmap = require("opdavies.keymap").nmap
+vim.keymap.set("n", "<M-h><M-l>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<M-h><M-m>", mark.add_file)
 
-nmap { "<M-h><M-l>", ui.toggle_quick_menu }
-nmap { "<M-h><M-m>", mark.add_file }
-
-nmap { "<leader>ha", mark.add_file }
-nmap { "<leader>hc", mark.clear_all }
-nmap { "<leader>ho", ui.toggle_quick_menu }
-nmap { "<leader>hr", mark.rm_file }
+vim.keymap.set("n", "<leader>ha", mark.add_file)
+vim.keymap.set("n", "<leader>hc", mark.clear_all)
+vim.keymap.set("n", "<leader>ho", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>hr", mark.rm_file)
 
 for i = 1, 5 do
-  nmap {
-    string.format("<space>%s", i),
-    function()
-      ui.nav_file(i)
-    end,
-  }
+  vim.keymap.set("n", string.format("<space>%s", i), function()
+    ui.nav_file(i)
+  end)
 end
