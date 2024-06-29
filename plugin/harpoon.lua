77aa4db95fcadcb8451022f6054cbabab986e665
Oliver Davies <oliver@oliverdavies.dev>
2024-06-29 15:13:32 +0100

    harpoon: simplify keybindings

--- plugin/harpoon.lua
+++ plugin/harpoon.lua
@@ -6,11 +6,6 @@ local ui = require "harpoon.ui"
 vim.keymap.set("n", "<M-h><M-l>", ui.toggle_quick_menu)
 vim.keymap.set("n", "<M-h><M-m>", mark.add_file)
 
-vim.keymap.set("n", "<leader>ha", mark.add_file)
-vim.keymap.set("n", "<leader>hc", mark.clear_all)
-vim.keymap.set("n", "<leader>ho", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>hr", mark.rm_file)
-
 for i = 1, 5 do
   vim.keymap.set("n", string.format("<space>%s", i), function()
     ui.nav_file(i)
