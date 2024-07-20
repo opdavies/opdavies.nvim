733f59570e69a9864f83c43dd9edc2cb6dda2eb7
Oliver Davies <oliver@oliverdavies.dev>
2024-07-20 12:53:55 +0100

    Update Telescope mappings

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -27,7 +27,21 @@ telescope.load_extension "ui-select"
 
 local builtin = require "telescope.builtin"
 
-local search_all_files = function()
+local M = {}
+
+M.diagnostics = function()
+  builtin.diagnostics { bufnr = 0 }
+end
+
+M.grep_bluecheese = function()
+  builtin.live_grep { cwd = "web/sites/default/themes/bluecheese" }
+end
+
+M.grep_drupalorg_theme = function()
+  builtin.live_grep { cwd = "web/themes/contrib/drupalorg_theme" }
+end
+
+M.search_all_files = function()
   builtin.find_files {
     find_command = { "rg", "--no-ignore", "--files" },
   }
@@ -38,10 +52,16 @@ vim.keymap.set("n", "<space>fb", builtin.buffers)
 vim.keymap.set("n", "<space>fd", builtin.find_files)
 vim.keymap.set("n", "<space>fg", builtin.live_grep)
 vim.keymap.set("n", "<space>fh", builtin.help_tags)
-vim.keymap.set("n", "<space>fi", search_all_files)
+vim.keymap.set("n", "<space>fi", M.search_all_files)
 vim.keymap.set("n", "<space>fk", builtin.keymaps)
 vim.keymap.set("n", "<space>ft", builtin.git_files)
 
+vim.keymap.set("n", "<space>dl", M.diagnostics)
 vim.keymap.set("n", "<space>ds", builtin.lsp_document_symbols)
 
 vim.keymap.set("n", "<space>gw", builtin.grep_string)
+
+vim.keymap.set("n", "<space>dgb", M.grep_bluecheese)
+vim.keymap.set("n", "<space>dgd", M.grep_drupalorg_theme)
+
+vim.keymap.set({ "n", "v" }, "<space>gw", builtin.grep_string)
